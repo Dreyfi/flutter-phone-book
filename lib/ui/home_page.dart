37bb5a4cc8208c5d6358330c09a60274860575a5
Dreyfi Ferreira <dreyfi.sferreira@gmail.com>
2019-06-25 00:15:00 -0300

    Criando o botão flutuante; obtendo a lista de contatos.

--- lib/ui/home_page.dart
+++ lib/ui/home_page.dart
@@ -10,8 +10,40 @@ class _HomePageState extends State<HomePage> {
 
   ContactHelper helper = ContactHelper();
 
+  List<Contact> contacts = List();
+
+  @override
+  void initState() {
+    super.initState();
+
+    helper.getAllContacts().then((list) {
+      setState(() {
+        contacts = list;
+      });
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("Contatos"),
+        backgroundColor: Colors.red,
+        centerTitle: true,
+      ),
+      backgroundColor: Colors.white,
+      floatingActionButton: FloatingActionButton(
+          onPressed: () {},
+        child: Icon(Icons.add),
+        backgroundColor: Colors.red,
+      ),
+      body: ListView.builder(
+        padding: EdgeInsets.all(10.0),
+        itemCount: contacts.length,
+        itemBuilder: (context, index) {
+
+        }
+      ),
+    );
   }
 }
